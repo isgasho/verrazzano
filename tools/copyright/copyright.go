234dd19cbeabcb2c1376e30e10e5429b193f8fd7
Mike Cico <michael.cico@oracle.com>
2021-02-22 18:01:34 -0500

    Remove author line per review comments

--- tools/copyright/copyright.go
+++ tools/copyright/copyright.go
@@ -1,7 +1,5 @@
 // Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-// Originally written in Java by Mark Nelson translated to Go by Mark Nelson and Mike Cico.
-
 package main
 
 import (
